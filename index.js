886b82a648d8d216254a5bab87add573d30f5d28
yanyongyu <yanyongyu_1@126.com>
2020-10-18 02:25:54 +0800

    :ambulance: fix reference error

--- index.js
+++ index.js
@@ -1,66 +1,72 @@
 const path = require("path");
 
 // Theme API.
-module.exports = (options, ctx) => ({
-  globalLayout: "./layouts/GlobalLayout.vue",
-  alias() {
-    const { themeConfig, siteConfig } = ctx;
-    // resolve algolia
-    const isAlgoliaSearch =
-      themeConfig.algolia ||
-      Object.keys((siteConfig.locales && themeConfig.locales) || {}).some(
-        (base) => themeConfig.locales[base].algolia
-      );
-    return {
-      "@AlgoliaSearchBox": isAlgoliaSearch
-        ? path.resolve(__dirname, "components/AlgoliaSearchBox.vue")
-        : path.resolve(__dirname, "noopModule.js"),
-    };
-  },
-  plugins: [
-    ["@vuepress/search", { searchMaxSuggestions: 10 }],
-    "@vuepress/nprogress",
-    [
-      "container",
-      {
-        type: "tip",
-        defaultTitle: {
-          "/": "TIP",
-          "/zh/": "提示",
+module.exports = (options, ctx) => {
+  const { themeConfig, siteConfig } = ctx;
+
+  // resolve algolia
+  const isAlgoliaSearch =
+    themeConfig.algolia ||
+    Object.keys((siteConfig.locales && themeConfig.locales) || {}).some(
+      (base) => themeConfig.locales[base].algolia
+    );
+
+  const enableSmoothScroll = themeConfig.smoothScroll === true;
+
+  return {
+    globalLayout: "./layouts/GlobalLayout.vue",
+    alias() {
+      return {
+        "@AlgoliaSearchBox": isAlgoliaSearch
+          ? path.resolve(__dirname, "components/AlgoliaSearchBox.vue")
+          : path.resolve(__dirname, "noopModule.js"),
+      };
+    },
+    plugins: [
+      ["@vuepress/search", { searchMaxSuggestions: 10 }],
+      "@vuepress/nprogress",
+      [
+        "container",
+        {
+          type: "tip",
+          defaultTitle: {
+            "/": "TIP",
+            "/zh/": "提示",
+          },
         },
-      },
-    ],
-    [
-      "container",
-      {
-        type: "warning",
-        defaultTitle: {
-          "/": "WARNING",
-          "/zh/": "注意",
+      ],
+      [
+        "container",
+        {
+          type: "warning",
+          defaultTitle: {
+            "/": "WARNING",
+            "/zh/": "注意",
+          },
         },
-      },
-    ],
-    [
-      "container",
-      {
-        type: "danger",
-        defaultTitle: {
-          "/": "WARNING",
-          "/zh/": "警告",
+      ],
+      [
+        "container",
+        {
+          type: "danger",
+          defaultTitle: {
+            "/": "WARNING",
+            "/zh/": "警告",
+          },
         },
-      },
+      ],
+      [
+        "container",
+        {
+          type: "details",
+          before: (info) =>
+            `<details class="custom-block details">${
+              info ? `<summary>${info}</summary>` : ""
+            }\n`,
+          after: () => "</details>\n",
+        },
+      ],
+      ["smooth-scroll", enableSmoothScroll],
     ],
-    [
-      "container",
-      {
-        type: "details",
-        before: (info) =>
-          `<details class="custom-block details">${
-            info ? `<summary>${info}</summary>` : ""
-          }\n`,
-        after: () => "</details>\n",
-      },
-    ],
-    ["smooth-scroll", themeConfig.smoothScroll === true],
-  ],
-});
+  };
+};
