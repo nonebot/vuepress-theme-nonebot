70fae35adf554516b1cd58add240b3af10d616e6
yanyongyu <yanyongyu_1@126.com>
2020-10-18 01:47:43 +0800

    :ambulance: fix navbar link exact active

--- components/NavLink.vue
+++ components/NavLink.vue
@@ -3,11 +3,11 @@
     v-if="isInternal"
     class="nav-link"
     :to="link"
-    :exact="exact"
+    :exact="true"
     @focusout.native="focusoutAction"
   >
     {{ item.text }}
-    <span v-if="item.subText" class="sub-text">{{item.subText}}</span>
+    <span v-if="item.subText" class="sub-text">{{ item.subText }}</span>
   </RouterLink>
   <a
     v-else
@@ -23,68 +23,70 @@
 </template>
 
 <script>
-import { isExternal, isMailto, isTel, ensureExt } from '../util'
+import { isExternal, isMailto, isTel, ensureExt } from "../util";
 
 export default {
-  name: 'NavLink',
+  name: "NavLink",
 
   props: {
     item: {
-      required: true
-    }
+      required: true,
+    },
   },
 
   computed: {
-    link () {
-      return ensureExt(this.item.link)
+    link() {
+      return ensureExt(this.item.link);
     },
 
-    exact () {
-      if (this.$site.locales) {
-        return Object.keys(this.$site.locales).some(rootLink => rootLink === this.link)
-      }
-      return this.link === '/'
+    // exact() {
+    //   if (this.$site.locales) {
+    //     return Object.keys(this.$site.locales).some(
+    //       (rootLink) => rootLink === this.link
+    //     );
+    //   }
+    //   return this.link === "/";
+    // },
+
+    isNonHttpURI() {
+      return isMailto(this.link) || isTel(this.link);
     },
 
-    isNonHttpURI () {
-      return isMailto(this.link) || isTel(this.link)
+    isBlankTarget() {
+      return this.target === "_blank";
     },
 
-    isBlankTarget () {
-      return this.target === '_blank'
+    isInternal() {
+      return !isExternal(this.link) && !this.isBlankTarget;
     },
 
-    isInternal () {
-      return !isExternal(this.link) && !this.isBlankTarget
-    },
-
-    target () {
+    target() {
       if (this.isNonHttpURI) {
-        return null
+        return null;
       }
       if (this.item.target) {
-        return this.item.target
+        return this.item.target;
       }
-      return isExternal(this.link) ? '_blank' : ''
+      return isExternal(this.link) ? "_blank" : "";
     },
 
-    rel () {
+    rel() {
       if (this.isNonHttpURI) {
-        return null
+        return null;
       }
       if (this.item.rel) {
-        return this.item.rel
+        return this.item.rel;
       }
-      return this.isBlankTarget ? 'noopener noreferrer' : ''
-    }
+      return this.isBlankTarget ? "noopener noreferrer" : "";
+    },
   },
 
   methods: {
-    focusoutAction () {
-      this.$emit('focusout')
-    }
-  }
-}
+    focusoutAction() {
+      this.$emit("focusout");
+    },
+  },
+};
 </script>
 
 <style lang="stylus">
